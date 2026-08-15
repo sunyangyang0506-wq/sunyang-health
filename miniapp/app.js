2289785ea48d9a6bca5bfc07fabed6510958164a
sunyangyang0506-wq <sunyangyang0506@gmail.com>
2026-08-15 08:56:54 +0800

    wire miniapp runtime config

--- miniapp/app.js
+++ miniapp/app.js
@@ -1,6 +1,18 @@
+let runtimeConfig = {
+  apiBase: '',
+  demoMode: true
+}
+
+try {
+  runtimeConfig = require('./env')
+} catch (err) {
+  // Production/local env.js is intentionally not committed.
+}
+
 App({
   globalData: {
-    apiBase: '',
-    demoMode: true
+    apiBase: runtimeConfig.apiBase || '',
+    demoMode: runtimeConfig.demoMode !== false,
+    authToken: wx.getStorageSync('healthAuthToken') || ''
   }
 })
